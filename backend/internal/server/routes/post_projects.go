80a80d870fbd985456427ea9769f317e67b07cf4
Malte Eiting <malte.eiting@offis.de>
2026-01-30 16:30:24 +0100

    fix(backend): proper reponses for invalid groups

--- backend/internal/server/routes/post_projects.go
+++ backend/internal/server/routes/post_projects.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 
@@ -89,6 +90,20 @@ func CreateProjectHandler(c echo.Context) error {
 		if err != nil || count == 0 {
 			return c.JSON(http.StatusForbidden, createProjectResponse{
 				Message: "You are not a member of this group",
+			})
+		}
+	}
+
+	_ , err = qtx.GetGroup(ctx, data.GroupID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return c.JSON(http.StatusNotFound, createProjectResponse{
+				Message: "Group not found",
+			})
+		} else {
+			logger.Error("Failed to get group", "err", err)
+			return c.JSON(http.StatusInternalServerError, createProjectResponse{
+				Message: "Internal server error",
 			})
 		}
 	}
@@ -338,6 +353,20 @@ func AddFilesToProjectHandler(c echo.Context) error {
 		}
 	}
 
+	_ , err = q.GetGroup(ctx, params.ProjectID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return c.JSON(http.StatusNotFound, addFilesResponse{
+				Message: "Group not found",
+			})
+		} else {
+			logger.Error("Failed to get group", "err", err)
+			return c.JSON(http.StatusInternalServerError, addFilesResponse{
+				Message: "Internal server error",
+			})
+		}
+	}
+
 	projectFiles := make([]*pgdb.ProjectFile, 0)
 	for key, name := range keys {
 		projectFile, err := q.AddFileToProject(ctx, pgdb.AddFileToProjectParams{
@@ -464,7 +493,9 @@ func QueryProjectHandler(c echo.Context) error {
 
 	user := c.(*middleware.AppContext).User
 	if user == nil {
-		return c.JSON(http.StatusUnauthorized, map[string]string{"error": "Unauthorized"})
+		return c.JSON(http.StatusUnauthorized, queryProjectResponse{
+			Message: "Unauthorized",
+		})
 	}
 
 	ctx := c.Request().Context()
@@ -477,7 +508,23 @@ func QueryProjectHandler(c echo.Context) error {
 			ID:     data.ProjectID,
 		})
 		if err != nil || count == 0 {
-			return c.JSON(http.StatusForbidden, map[string]string{"error": "Unauthorized"})
+			return c.JSON(http.StatusForbidden, queryProjectResponse{
+				Message: "Unauthorized",
+			})
+		}
+	}
+
+	_ , err := q.GetGroup(ctx, data.ProjectID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return c.JSON(http.StatusNotFound, queryProjectResponse{
+				Message: "Group not found",
+			})
+		} else {
+			logger.Error("Failed to get group", "err", err)
+			return c.JSON(http.StatusInternalServerError, queryProjectResponse{
+				Message: "Internal server error",
+			})
 		}
 	}
 
@@ -603,7 +650,10 @@ func QueryProjectStreamHandler(c echo.Context) error {
 
 	user := c.(*middleware.AppContext).User
 	if user == nil {
-		return c.JSON(http.StatusUnauthorized, map[string]string{"error": "Unauthorized"})
+		return c.JSON(http.StatusUnauthorized, streamResponse{
+			Message: "Unauthorized",
+			Data:    []responseData{},
+		})
 	}
 
 	ctx := c.Request().Context()
@@ -616,7 +666,26 @@ func QueryProjectStreamHandler(c echo.Context) error {
 			ID:     data.ProjectID,
 		})
 		if err != nil || count == 0 {
-			return c.JSON(http.StatusForbidden, map[string]string{"error": "Unauthorized"})
+			return c.JSON(http.StatusForbidden, streamResponse{
+				Message: "Unauthorized",
+				Data:    []responseData{},
+			})
+		}
+	}
+
+	_ , err := q.GetGroup(ctx, data.ProjectID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return c.JSON(http.StatusNotFound, streamResponse{
+				Message: "Group not found",
+				Data:    []responseData{},
+			})
+		} else {
+			logger.Error("Failed to get group", "err", err)
+			return c.JSON(http.StatusInternalServerError, streamResponse{
+				Message: "Internal server error",
+				Data:    []responseData{},
+			})
 		}
 	}
 
