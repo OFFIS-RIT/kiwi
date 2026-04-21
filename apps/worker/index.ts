4cf4a90e3465996cce33c4e1d842368b4241542c
Malte Eiting <malte.eiting@offis.de>
2026-04-21 12:25:47 +0200

    fix: worker ai scale

--- apps/worker/index.ts
+++ apps/worker/index.ts
@@ -4,10 +4,10 @@ import { configureAIConcurrency } from "@kiwi/ai";
 import { env } from "./env";
 
 configureAIConcurrency({
-    text: env.AI_TEXT_CONCURRENCY,
-    image: env.AI_IMAGE_CONCURRENCY,
-    embedding: env.AI_EMBEDDING_CONCURRENCY,
-    audio: env.AI_AUDIO_CONCURRENCY,
+    text: env.AI_TEXT_CONCURRENCY * env.WORKER_CONCURRENCY,
+    image: env.AI_IMAGE_CONCURRENCY * env.WORKER_CONCURRENCY,
+    embedding: env.AI_EMBEDDING_CONCURRENCY * env.WORKER_CONCURRENCY,
+    audio: env.AI_AUDIO_CONCURRENCY * env.WORKER_CONCURRENCY,
 });
 
 export const backend = await BackendPostgres.connect(env.DATABASE_DIRECT_URL);
