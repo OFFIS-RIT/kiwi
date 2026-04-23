2dc0f1247b232e2df52d9a154b051d207690fb2c
Malte Eiting <malte.eiting@offis.de>
2026-04-23 10:13:49 +0200

    fix: missing deps in migrations image

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,6 +5,6 @@ export default defineConfig({
     schema: "./packages/db/src/tables/*",
     dialect: "postgresql",
     dbCredentials: {
-        url: process.env.DATABASE_DIRECT_URL as string,
+        url: (process.env.DATABASE_DIRECT_URL ?? process.env.DATABASE_URL) as string,
     },
 });
